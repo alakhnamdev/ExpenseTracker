6e021300bb8880ee45e37e03d0b272ee50d143b4
alakhnamdev <namdev.alakh@gmail.com>
2025-09-28 22:10:46 +0530

    Updated login.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,7 +2,7 @@ const config = {
     app_name: "Expense Tracker Backend",
     app_description: "A robust and scalable backend service. Created By - Alakh Namdev",
     debug: process.env.NODE_ENV !== 'production',
-    allowed_origins: [process.env.ALLOWED_ORIGINS] // Allow specific origin for CORS
+    allowed_origins: [`${process.env.ALLOWED_ORIGINS}`] // Allow specific origin for CORS
 };
 
 module.exports = config;
